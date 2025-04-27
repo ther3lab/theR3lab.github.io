9a7a14a6ec5082ec9676735b8bcee1454049e373
ther3lab <ther3lab@gmail.com>
2025-04-27 22:51:33 +0000

    Deploying to gh-pages from @ ther3lab/ther3lab.github.io@506e27740578141d5c1e49f1adcd5e449b925510 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -60,11 +60,6 @@ ninja.data = [{
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project.html";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project.html";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
